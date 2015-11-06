a9e81b0e7b6c632777502b5b070768ccbf3b0a12
Zoher Ghadyali <zoher.ghadyali@gmail.com>
2015-11-05 19:35:29 -0500

    Facebook login works now but doesn't return anything

--- app/src/main/java/com/example/zghadyali/carshareapp/MainActivity.java
+++ app/src/main/java/com/example/zghadyali/carshareapp/MainActivity.java
@@ -1,21 +1,28 @@
 package com.example.zghadyali.carshareapp;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
 import com.facebook.FacebookSdk;
 
 public class MainActivity extends AppCompatActivity {
 
+    public loginFacebook loginfb;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+        loginfb = new loginFacebook();
+        transitionToFragment(loginfb);
     }
 
     public void transitionToFragment(Fragment fragment){
@@ -27,6 +34,11 @@ public class MainActivity extends AppCompatActivity {
         transaction.commit();
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        loginfb.callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/com/example/zghadyali/carshareapp/loginFacebook.java
+++ app/src/main/java/com/example/zghadyali/carshareapp/loginFacebook.java
@@ -2,41 +2,49 @@ package com.example.zghadyali.carshareapp;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.Profile;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class loginFacebook extends Fragment {
 
-    LoginButton loginButton;
-    CallbackManager callbackManager;
+    public LoginButton loginButton;
+    public CallbackManager callbackManager;
     @Override
     public View onCreateView(
             LayoutInflater inflater,
             ViewGroup container,
             Bundle savedInstanceState) {
-        View rootview = inflater.inflate(R.layout.loginFacebook, container, false);
+        View rootview = inflater.inflate(R.layout.login_facebook, container, false);
 
         loginButton = (LoginButton) rootview.findViewById(R.id.login_button);
-        loginButton.setReadPermissions("user_friends");
         // If using in a fragment
         loginButton.setFragment(this);
         // Other app specific specialization
 
+        callbackManager = CallbackManager.Factory.create();
         // Callback registration
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 // App code
+                Log.d("Access token: ", AccessToken.getCurrentAccessToken().toString());
+                Log.d("Profile: ", Profile.getCurrentProfile().toString());
             }
 
             @Override
@@ -48,7 +56,19 @@ public class loginFacebook extends Fragment {
             public void onError(FacebookException exception) {
                 // App code
             }
+
+
+
         });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginManager.getInstance().logInWithReadPermissions(getActivity(), Arrays.asList("public_profile", "user_friends"));
+            }
+        });
+
+
 
     return rootview;
     }
