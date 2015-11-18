cf5436c091636136722e4da71fca1a13d0dca110
yunhsincynthiachen <yun-hsin.chen@students.olin.edu>
2015-11-18 18:10:24 -0500

    volley stuff for pushing names to approved list

--- app/src/main/java/com/example/zghadyali/carshareapp/VolleyRequests.java
+++ app/src/main/java/com/example/zghadyali/carshareapp/VolleyRequests.java
@@ -10,8 +10,10 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 /**
@@ -45,6 +47,36 @@ public class VolleyRequests {
                 Request.Method.POST,
                 url,
                 CarInfo,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Error!", error.getMessage());
+                    }
+                });
+
+        queue.add(request);
+
+    }
+
+    public void addtoapproved(String id_name, JSONArray approved_people){
+        String url = "http://52.33.226.47/cars/" + id_name + "/approved";
+        JSONObject approved_users = new JSONObject();
+
+        try{
+            approved_users.put("user", approved_people);
+        } catch (Exception e){
+            Log.e("ERROR!", e.getMessage());
+        }
+        Log.d("JSONObject: ", approved_users.toString());
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.POST,
+                url,
+                approved_users,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
