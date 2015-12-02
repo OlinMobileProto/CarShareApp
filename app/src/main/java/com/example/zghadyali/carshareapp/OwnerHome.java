202689691b8c9f08285fca37c5740d509f138ae7
yunhsincynthiachen <yun-hsin.chen@students.olin.edu>
2015-12-01 20:35:31 -0500

    with getting car info in the ownerhome fragment

--- app/src/main/java/com/example/zghadyali/carshareapp/OwnerHome.java
+++ app/src/main/java/com/example/zghadyali/carshareapp/OwnerHome.java
@@ -2,11 +2,14 @@ package com.example.zghadyali.carshareapp;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import org.json.JSONObject;
 
 /**
  * Created by Jordan on 11/18/15.
@@ -22,6 +25,16 @@ public class OwnerHome extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        VolleyRequests handler = new VolleyRequests(getActivity().getApplicationContext());
+        
+        handler.getcarinfo(new callback_cars() {
+            @Override
+            public void callback(JSONObject cars) {
+                Log.d("JSON CAR to string: ", cars.toString());
+            }
+        }, "1090612884283818");
+
+
         view = inflater.inflate(R.layout.owner_home, container, false);
         carLocation = (TextView)view.findViewById(R.id.car_location);
         editCarLocation = (EditText)view.findViewById(R.id.car_location_edit);
