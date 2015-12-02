dfef8cd9048a7c3ae11377364f3c26550da87437
Jordan Van Duyne <jordannn808@gmail.com>
2015-12-01 19:12:07 -0500

    add the fb accesstoken stuff

--- app/src/main/java/com/example/zghadyali/carshareapp/OwnerActivity.java
+++ app/src/main/java/com/example/zghadyali/carshareapp/OwnerActivity.java
@@ -5,8 +5,11 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.facebook.AccessToken;
 
 /**
  * Created by Jordan on 11/18/15.
@@ -16,10 +19,24 @@ public class OwnerActivity extends AppCompatActivity{
     public OwnerHome ownerHome = new OwnerHome();
     public OwnerSettings ownerSettings = new OwnerSettings();
 
+    public AccessToken accessToken;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_owner);
+
+        accessToken = AccessToken.getCurrentAccessToken();
+        Log.d("LOGGEDIN ACCESS TOKEN: ", accessToken.getToken());
+        if (getIntent().hasExtra("profile_id") && getIntent().hasExtra("name")){
+            String profile_id = getIntent().getExtras().getString("profile_id");
+            String name = getIntent().getExtras().getString("name");
+            Log.d("PROFILE ID: ", profile_id);
+            Log.d("name", name);
+        }
+        else{
+            Log.d("OWNER CLASS: ", "I don't have any of that information right now");
+        }
 
         ownerHome.setArguments(getIntent().getExtras());
         getSupportFragmentManager().beginTransaction().add(R.id.container, ownerHome).commit();
