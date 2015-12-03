80383a9d4e5503c8079c5ea0133ff72061c69765
Jordan Van Duyne <jordannn808@gmail.com>
2015-12-03 13:18:34 -0500

    add volley requests to owner settings

--- app/src/main/java/com/example/zghadyali/carshareapp/OwnerSettings.java
+++ app/src/main/java/com/example/zghadyali/carshareapp/OwnerSettings.java
@@ -2,11 +2,17 @@ package com.example.zghadyali.carshareapp;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by Jordan on 12/1/15.
@@ -20,6 +26,11 @@ public class OwnerSettings extends Fragment {
     private EditText editCarModel;
     private TextView licensePlate;
     private EditText editLicensePlate;
+    private Spinner transmissionSpinner;
+    private Button updateButton;
+
+    private String profile_id;
+    private JSONObject cars;
 
 
     @Override
@@ -32,7 +43,39 @@ public class OwnerSettings extends Fragment {
         editCarModel = (EditText)view.findViewById(R.id.settings_car_model_edit);
         licensePlate = (TextView)view.findViewById(R.id.settings_license_plate);
         editLicensePlate = (EditText)view.findViewById(R.id.settings_license_plate_edit);
+        transmissionSpinner = (Spinner)view.findViewById(R.id.settings_transmission_spinner);
+        updateButton = (Button)view.findViewById(R.id.settings_update_button);
 
+        cars = ((OwnerActivity)getActivity()).car_info;
+        profile_id = ((OwnerActivity)getActivity()).profile_id;
+        Log.d("Stuff:", cars.toString());
+
+        try {
+            editCarMake.setText(cars.getString("make"));
+            editCarModel.setText(cars.getString("model"));
+            editLicensePlate.setText(cars.getString("licensePlate"));
+        } catch (JSONException e) {
+            Log.e("MYAPP", "unexpected JSON exception", e);
+            // Do something to recover ... or kill the app.
+        }
+
+
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                VolleyRequests handler = new VolleyRequests(getActivity().getApplicationContext());
+                JSONObject newCarInfo = new JSONObject();
+                try {
+                    newCarInfo.put("make", editCarMake.getText().toString());
+                    newCarInfo.put("model", editCarModel.getText().toString());
+                    newCarInfo.put("licensePlate", editLicensePlate.getText().toString());
+                } catch (JSONException e) {
+                    Log.e("MYAPP", "unexpected JSON exception", e);
+                    // Do something to recover ... or kill the app.
+                }
+                handler.addcarinfo(profile_id, newCarInfo);
+            }
+        });
 
         return view;
     }
