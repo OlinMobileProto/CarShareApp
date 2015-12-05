4ea099a1d284d8a586793882420be0d84c9490f4
Zoher Ghadyali <zoher.ghadyali@gmail.com>
2015-12-05 12:25:13 -0500

    Started borrower home fragment and got list of cars borrower can use

--- app/src/main/java/com/example/zghadyali/carshareapp/BorrowerActivity.java
+++ app/src/main/java/com/example/zghadyali/carshareapp/BorrowerActivity.java
@@ -1,5 +1,8 @@
 package com.example.zghadyali.carshareapp;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -26,6 +29,8 @@ public class BorrowerActivity extends AppCompatActivity {
     public JSONObject borrower_cars;
     public JSONArray car_ids;
     public JSONArray carsJSON;
+    public ArrayList<String> carsList;
+    public BorrowerHome borrowerHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,22 +53,38 @@ public class BorrowerActivity extends AppCompatActivity {
                     car_ids = new JSONArray();
                     try{
                         car_ids = borrower_cars.getJSONArray("can_borrow");
-
-                        int len = car_ids.length();
+                        final int len = car_ids.length();
+                        Log.d("You are approved for", String.valueOf(len));
                         carsJSON = new JSONArray();
+                        carsList = new ArrayList<String>();
                         for (int i = 0; i < len; i++) {
                             String test = car_ids.getString(i);
                             handler.getcarinfo(new callback_cars() {
                                 @Override
                                 public void callback(JSONObject cars) {
                                     carsJSON.put(cars);
-                                    Log.d("BORROWER'S CARS", carsJSON.toString());
+                                    try{
+                                        String temp_name = cars.getString("owner");
+                                        String temp_make = cars.getString("make");
+                                        String temp_model = cars.getString("model");
+                                        if (temp_make != null && temp_model != null) {
+                                            carsList.add(temp_name + "'s " + temp_make + " " + temp_model + " "); //then need to add year
+                                        } else if (temp_model != null){
+                                            carsList.add(temp_name + "'s " + temp_model);
+                                        } else if (temp_make != null){
+                                            carsList.add(temp_name + "'s " + temp_make);
+                                        } else{
+                                            carsList.add(temp_name + "'s Car");
+                                        }
+                                    } catch (Exception e){
+                                        e.getMessage();
+                                    }
+                                    Log.d("BORROWER'S CARS", carsList.toString());
+                                    borrowerHome = new BorrowerHome();
+                                    transitionToFragment(borrowerHome);
                                 }
                             }, test);
-
                         }
-
-
                     } catch (Exception e){
                         Log.e("Error: ", e.getMessage());
                     }
@@ -84,7 +105,7 @@ public class BorrowerActivity extends AppCompatActivity {
                                 Log.d("USER ID JSON", user_id.toString());
                                 name = user_id.getString("name");
                                 profile_id = user_id.getString("id");
-                                VolleyRequests handler = new VolleyRequests(getApplicationContext());
+                                final VolleyRequests handler = new VolleyRequests(getApplicationContext());
                                 handler.getborrowerinfo(new callback_cars() {
                                     @Override
                                     public void callback(JSONObject cars) {
@@ -93,6 +114,38 @@ public class BorrowerActivity extends AppCompatActivity {
                                         car_ids = new JSONArray();
                                         try{
                                             car_ids = borrower_cars.getJSONArray("can_borrow");
+                                            final int len = car_ids.length();
+                                            Log.d("You are approved for", String.valueOf(len));
+                                            carsJSON = new JSONArray();
+                                            carsList = new ArrayList<String>();
+                                            for (int i = 0; i < len; i++) {
+                                                String test = car_ids.getString(i);
+                                                handler.getcarinfo(new callback_cars() {
+                                                    @Override
+                                                    public void callback(JSONObject cars) {
+                                                        carsJSON.put(cars);
+                                                        try{
+                                                            String temp_name = cars.getString("owner");
+                                                            String temp_make = cars.getString("make");
+                                                            String temp_model = cars.getString("model");
+                                                            if (temp_make != null && temp_model != null) {
+                                                                carsList.add(temp_name + "'s " + temp_make + " " + temp_model + " "); //then need to add year
+                                                            } else if (temp_model != null){
+                                                                carsList.add(temp_name + "'s " + temp_model);
+                                                            } else if (temp_make != null){
+                                                                carsList.add(temp_name + "'s " + temp_make);
+                                                            } else{
+                                                                carsList.add(temp_name + "'s Car");
+                                                            }
+                                                        } catch (Exception e){
+                                                            e.getMessage();
+                                                        }
+                                                        Log.d("BORROWER'S CARS", carsList.toString());
+                                                        borrowerHome = new BorrowerHome();
+                                                        transitionToFragment(borrowerHome);
+                                                    }
+                                                }, test);
+                                            }
                                         } catch (Exception e){
                                             Log.e("Error: ", e.getMessage());
                                         }
@@ -127,4 +180,14 @@ public class BorrowerActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void transitionToFragment(Fragment fragment){
+        //This function takes as input a fragment, initializes the fragment manager and replaces
+        //the container with the provided fragment
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.commit();
+    }
+
 }
---
+++ app/src/main/java/com/example/zghadyali/carshareapp/BorrowerHome.java
@@ -0,0 +1,44 @@
+package com.example.zghadyali.carshareapp;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+public class BorrowerHome extends Fragment {
+
+    public Button now;
+    public ListView carsListView;
+    public ArrayAdapter carsAdapter;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootview = inflater.inflate(R.layout.fragment_borrower_home, container, false);
+
+        now = (Button) rootview.findViewById(R.id.now_button);
+        now.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        carsListView = (ListView) rootview.findViewById(R.id.cars_list);
+        carsAdapter = new ArrayAdapter<String>(getActivity(), R.layout.text_view, ((BorrowerActivity)getActivity()).carsList);
+        carsListView.setAdapter(carsAdapter);
+
+
+        return rootview;
+    }
+
+
+
+}
