d164231031f56623ff034aef61795673e4553839
Bill Wong <billmwong@gmail.com>
2015-12-12 13:16:21 -0500

    added string resources

--- app/src/main/java/com/example/zghadyali/carshareapp/Owner/UpdateApprovedList.java
+++ app/src/main/java/com/example/zghadyali/carshareapp/Owner/UpdateApprovedList.java
@@ -33,7 +33,7 @@ public class UpdateApprovedList extends setALParent {
             ownerActivity = (OwnerActivity) thisActivity;
         }
 
-        doneButton.setText("Update");
+        doneButton.setText(R.string.update);
         // Hide login button
         loginButton = (LoginButton) view.findViewById(R.id.login_button);
         loginButton.setVisibility(View.GONE);
