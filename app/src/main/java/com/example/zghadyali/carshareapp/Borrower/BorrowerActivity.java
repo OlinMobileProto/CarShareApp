4dd145f6803baf8ef464989ba63b2b384ed29353
Bill Wong <billmwong@gmail.com>
2015-12-18 17:38:18 -0500

    fixed appcompat issue for borrower

--- app/src/main/java/com/example/zghadyali/carshareapp/Borrower/BorrowerActivity.java
+++ app/src/main/java/com/example/zghadyali/carshareapp/Borrower/BorrowerActivity.java
@@ -2,6 +2,7 @@ package com.example.zghadyali.carshareapp.Borrower;
 
 import android.content.Intent;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
@@ -32,7 +33,7 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
 
-public class BorrowerActivity extends AppCompatActivity {
+public class BorrowerActivity extends FragmentActivity {
 
     public AccessToken accessToken;
     public String profileID;
